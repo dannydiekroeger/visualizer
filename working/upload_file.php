c008cc89c8804671265a4b679a80575d95dd3b5c
Will Han <whan1@stanford.edu>
2014-02-19 15:04:41 -0800

    upload file.php

---
+++ working/upload_file.php
@@ -0,0 +1,25 @@
+<?php
+$allowedExts = array("mp3", "mpeg", "wav");
+$temp = explode(".", $_FILES["file"]["name"]);
+$extension = end($temp);
+if ((($_FILES["file"]["type"] == "audio/mpeg")
+|| ($_FILES["file"]["type"] == "audio/wav"))
+&& in_array($extension, $allowedExts))
+  {
+  if ($_FILES["file"]["error"] > 0)
+    {
+    echo "Error: " . $_FILES["file"]["error"] . "<br>";
+    }
+  else
+    {
+    echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+    echo "Type: " . $_FILES["file"]["type"] . "<br>";
+    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+    echo "Stored in: " . $_FILES["file"]["tmp_name"];
+    }
+  }
+else
+  {
+  echo "Invalid file";
+  }
+?>
