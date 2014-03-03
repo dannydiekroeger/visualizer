974bfede48e0b5d4ae5a638ffb4951cb097a4434
Danny <dannydiekroeger@gmail.com>
2014-03-03 11:52:51 -0800

    flux

--- flux/flux.js
+++ flux/flux.js
@@ -5,22 +5,33 @@
 //         - refers to "ctx" which is defined in sound.js 
 
 var maxAmp;
-var imageData;
-var newimageData;
+var fluxImageData;
+var newfluxImageData;
 var closestNotes;
 var width;
 var height;
-var im;
+var fluxim;
 var invert;
 var opacityScale;
 var pixels;
-var centX;
-var centY;
-var imageDataArray;
-var livePixels;
-var deadPixels;
+var fluxCentX;
+var fluxCentY;
+var fluxImageDataArray;
+var fluxLivePixels;
+var fluxDeadPixels;
 var oldNoteCounts;
 var velo;
+var fluxgravity;
+var fluxheightscale;
+var fluxRadialGrav;
+var fluxRotationTheta;
+var fluxradscale;
+var fluxRotation;
+var fluxRadius;
+var fluxRandMiddle;
+var fluxArrayOffset;
+var fluxUpdatePixels;
+var fluxUpdatePixelsVal;
 
 function initFlux() {
 		var imsrc = "frac2.jpg";
@@ -34,22 +45,30 @@ function initFlux() {
 		    canv.setAttribute("style", "background:black");
 		    imsrc = "images/"+imsrc;
 		}
-	    centX = canv.width/2.0;
-	    centY = canv.height/2.0;
+	    fluxCentX = canv.width/2.0;
+	    fluxCentY = canv.height/2.0;
 	    initKeyboard();
-	    im = new Image();
-		im.onload = imageLoadedFlux;
-		im.src = imsrc
+	    fluxim = new Image();
+		fluxim.onload = imageLoadedFlux;
+		fluxim.src = imsrc
 		
-
+		fluxRandMiddle = false;
+		fluxArrayOffset = 100;
+		fluxUpdatePixels = updateFluxRadial;
+		fluxUpdatePixelsVal = 1;
 		//canv.style.backgroundImage=im;
 		opacityScale = 10;
 		maxAmp = 1.0;
 		invert = false;
 		velo = 1;
-		
-		
-		//imageData = ctx.getImageData(0,0,canv.width,canv.height);
+		fluxgravity = 1;
+		fluxRadialGrav = 2;
+		fluxheightscale = 10;
+		fluxradscale = 10;
+		fluxRotationTheta = 0;
+		fluxRotation = .01;
+		fluxRadius = 100;
+		//fluxImageData = ctx.getfluxImageData(0,0,canv.width,canv.height);
 }
 
 function loadFlux() {
@@ -59,48 +78,41 @@ function loadFlux() {
 }
 
 function imageLoadedFlux(ev) {
-    im = ev.target; // the image
+    fluxim = ev.target; // the image
 
     // read the width and height of the canvas
     width = canv.width;
     height = canv.height;
     // stamp the image on the left of the canvas:
-    ctx.drawImage(im, 0, 0);
+    ctx.drawImage(fluxim, 0, 0);
     // get all canvas pixel data
 	initLiveAndDead();
-    imageData = ctx.getImageData(0, 0, width, height);
-    //ctx.putImageData(imageData, 0, 0);
+    fluxImageData = ctx.getImageData(0, 0, width, height);
+    //ctx.putfluxImageData(fluxImageData, 0, 0);
     for (y = 0; y < height; y++) {
 		inpos = y * width * 4; // *4 for 4 ints per pixel
 	    for (x = 0; x < width; x++) {
 	    		var index = inpos;
-	        	var r = imageData.data[inpos++];
-	        	var g = imageData.data[inpos++];
-	        	var b = imageData.data[inpos++];
-	        	var alpha = imageData.data[inpos];
-	        	var note = getClosestNote([r,g,b]);
+	        	var r = fluxImageData.data[inpos++];
+	        	var g = fluxImageData.data[inpos++];
+	        	var b = fluxImageData.data[inpos++];
+	        	var alpha = fluxImageData.data[inpos];
+	        	//var note = getClosestNote([r,g,b]);
 	        	var pixel = new Object();
 	        	//pixel.r = r;
 	        	//pixel.g = g;
 	        	//pixel.b = b;
 	        	//pixel.alpha = alpha;
-	        	pixel.note = note;
+	        	//pixel.note = note;
 	        	pixel.index = index;
 	        	pixel.x = x;
 	        	pixel.y = y;
-	        	if(pixel.x>centX) {
-	        		pixel.velocity = [5,0];
-	        		pixel.hand = 1
-	        	}
-	        	else {
-	        		pixel.velocity = [-5,0];
-	        		pixel.hand = 0;
-	        	}
+	        	pixel.velocity = [0,0];
 	        	pixel.coord = getCoord(pixel);
-	        	pixel.distanceFromCenter = getDistanceFromCenter(x,y);
+				pixel = setRadAndTheta(pixel);
 	        	if(!isBlack(pixel)) {
-	        		deadPixels.push(pixel);
-	        		imageData.data[pixel.index+3] = 0;
+	        		fluxDeadPixels.push(pixel);
+	        		fluxImageData.data[pixel.index+3] = 0;
 	        	}
 	        	inpos++;
 	    }
@@ -108,66 +120,220 @@ function imageLoadedFlux(ev) {
    	initFluxPixels();
 }
 
+function setRadAndTheta(pixel) {
+	pixel.radius = getDistanceFromCenter(pixel.x,pixel.y);
+	pixel.theta = Math.asin(pixel.coord["y"]/pixel.radius);
+	if(pixel.coord["x"] < 0) {
+		pixel.theta *= -1;
+	    pixel.radius = (-1)*pixel.radius;
+	}
+	return pixel;
+}
+
 function calculateIndex(pixel) {
 	return pixel.y*canv.width*4 + pixel.x*4;
 }
 
 function updateFlux(array, something, beat) {
-	//if(livePixels.length > 1)console.log(livePixels[0].index);
-	if(beat) velo += 100;
-	updateFluxPixels();
-	ctx.putImageData(imageData,0,0);
+	//if(fluxLivePixels.length > 1)console.log(fluxLivePixels[0].index);
+	//if(beat) velo += 100;
+	fluxUpdatePixels(array);
+	ctx.putImageData(fluxImageData,0,0);
 }
 
-function updateFluxPixels() {
-	var newLivePixels = new Array();
-	//console.log(livePixels);
-	for(var i=0;i<livePixels.length;i++){
-		var pixel = livePixels[i];
-		imageData.data[pixel.index+3]=0;
+function updateFluxPixelsSpawn(array) {
+	var newfluxLivePixels = new Array();
+	//console.log(fluxLivePixels);
+	for(var i=0;i<fluxLivePixels.length;i++){
+		var pixel = fluxLivePixels[i];
+		fluxImageData.data[pixel.index+3]=0;
 		var newPixel = moveStep(pixel);
-		newPixel.velocity = getVelocity(newPixel);
-		newLivePixels.push(newPixel);
-		imageData.data[pixel.index+3]=255;
+		//newPixel.velocity = getVelocity(newPixel);
+		if(newPixel.y <= canv.height) {
+			newPixel.velocity = getVelocity(newPixel,array);
+			newfluxLivePixels.push(newPixel);
+			fluxImageData.data[pixel.index+3]=255;
+		}
 	}
-	livePixels = newLivePixels;
+	
+	var chunksize = canv.width/array.length;
+	for(var i=0; i<array.length;i++) {
+		var x = Math.floor(i*chunksize);
+		var value = array[i];
+		var numPix = 0;
+		if(value>150) numPix = 1;
+		for(var j=0;j<numPix;j++) {
+			var newPixel = new Object();
+			newPixel.x = x;
+			newPixel.y = canv.height;
+			newPixel.velocity = [getRandZeroOne(),(-1)*Math.floor(array[i]/fluxheightscale)-j];
+			newPixel.index = calculateIndex(newPixel);
+			newfluxLivePixels.push(newPixel);
+			fluxImageData.data[newPixel.index+3]=255;
+		}
+	}
+	
+	fluxLivePixels = newfluxLivePixels;	
 }
 
-function getVelocity(pixel) {
-	var scale = 50;
-	//return [Math.log(pixel.coord["x"])/Math.log(1000), Math.log(pixel.coord["y"])/Math.log(1000)];
-	if(velo > 15) velo-=15;
-	if(pixel.hand == 1) return [velo,0];
-	return[-velo,0];
+function getRandZeroOne(){
+	var rand = Math.random();
+	if(rand > .66) return 1;
+	else if(rand > .33) return 0;
+	return -1;
 }
 
+function updateFluxPixels(array) {
+	var newfluxLivePixels = new Array();
+	//console.log(fluxLivePixels);
+	for(var i=0;i<fluxLivePixels.length;i++){
+		var pixel = fluxLivePixels[i];
+		fluxImageData.data[pixel.index+3]=0;
+		var newPixel = pixel;
+		newPixel.velocity = getVelocity(newPixel,array);
+		newPixel = moveStep(newPixel);
+		newfluxLivePixels.push(newPixel);
+		fluxImageData.data[pixel.index+3]=255;
+	}
+	fluxLivePixels = newfluxLivePixels;
+}
+
+function updateFluxRadial(array){
+	var chunksize = Math.PI/(array.length-fluxArrayOffset);
+	var newfluxLivePixels = new Array();
+	for(var i=0;i<fluxLivePixels.length;i++){
+		var pixel = fluxLivePixels[i];
+		fluxImageData.data[pixel.index+3]=0;
+		var newPixel = pixel;
+		//newPixel.velocity = getVelocity(newPixel);
+		//newPixel.velocity = getVelocity(newPixel,array);
+		newPixel.velocity = getNewRadialVelocity(newPixel);
+		if(Math.abs(newPixel.radius)<fluxRadius) {
+			var trueTheta = newPixel.theta + Math.PI/2;
+			if(newPixel.radius < 0)trueTheta = Math.abs(trueTheta - Math.PI);
+			var index = Math.floor(trueTheta/chunksize);
+			if(index > array.length-1)index = array.length-1;
+			var value = array[index]/fluxradscale;
+			if(newPixel.radius < 0)newPixel.velocity[0] -= value;
+			else newPixel.velocity[0] += value;
+		}
+		newPixel = moveRadialStep(newPixel)
+		newfluxLivePixels.push(newPixel);
+		fluxImageData.data[pixel.index+3]=255;
+	}
+	fluxLivePixels = newfluxLivePixels;
+}
+
+function getNewRadialVelocity(pixel) {
+	var thetavelo = 0;
+	//var thetavelo = pixel.velocity[1];
+	if(Math.abs(pixel.radius) > fluxRadius) {
+		if(pixel.radius > 0) {
+			return [pixel.velocity[0]-fluxRadialGrav,thetavelo];
+		} else {
+			return [pixel.velocity[0]+fluxRadialGrav,thetavelo];	
+		}
+	} else {
+		var skew = 0;
+		var rand = 0;
+		if(fluxRandMiddle)rand = getRandZeroOne();
+		if(pixel.radius < 0) skew = 0;
+		return [pixel.velocity[0]/1.5+rand +skew,fluxRotationTheta];
+	}	
+}
+
+function flipR(pixel) {
+	pixel.radius = (-1)*pixel.radius;
+	pixel.velocity[0] = (-1)*pixel.velocity[0];
+	return pixel;
+}
+
+// velocity is now [r,theta]
+function moveRadialStep(pixel) {
+	pixel.radius += pixel.velocity[0];
+	var newTheta = pixel.theta+pixel.velocity[1];
+	if(newTheta > Math.PI/2) {
+		newTheta = (-1)*Math.PI + newTheta;
+		pixel = flipR(pixel);
+	} else if(newTheta < (-1)*Math.PI/2) {
+		newTheta = Math.PI + newTheta;
+		pixel = flipR(pixel);
+	}
+	pixel.theta = newTheta;
+	return updatePixelFromRad(pixel);
+}
+
+// sets up the rest of pixel's values to correspond with
+// new radius and theta
+function updatePixelFromRad(pixel) {
+	var xCoord = Math.floor(pixel.radius * Math.cos(pixel.theta));
+	var yCoord = Math.floor(pixel.radius * Math.sin(pixel.theta));
+	pixel.coord = {"x":xCoord,"y":yCoord};
+	pixel.x = xCoord+fluxCentX;
+	pixel.y = yCoord+fluxCentY;
+	/*For Respawn in center
+	if(pixel.x > canvas.width || pixel.x < 0 || pixel.y > canvas.height || pixel.y < 0) {
+		pixel.x = fluxCentX;
+		pixel.y = fluxCentY;
+		pixel.coord = {"x":0,"y":0};
+		pixel.radius = 0;
+	}*/
+	pixel.index = calculateIndex(pixel);
+	return pixel;
+}
+
+
+function getVector(startx,starty,endx,endy,scale) {
+	var dx = endx-startx;
+	var dy = endy-starty;
+	var mag = Math.sqrt(dx*dx+dy*dy);
+	return [Math.floor(dx*scale/mag),Math.floor(dy*scale/mag)];
+}
+
+function getVelocity(pixel,array) {
+	var xvel = pixel.velocity[0];
+	if(pixel.x >= canv.width || pixel.x <= 0) xvel = -xvel;
+	if(pixel.y >= canv.height - 200) {
+		var yval = getBump(pixel,array);
+		/*
+		var variance = -1;
+		var rand = Math.random();
+		if(rand > .66) variance = 0;
+		else if(rand > .33) variance = 1;
+		return [pixel.velocity[0],-pixel.velocity[1]+variance-1];
+		*/
+		//if(Math.random() > .9)
+		//return [getRandZeroOne(),getRandZeroOne()-yval];
+		return [0,getRandZeroOne()-yval];
+		//else return [getRandZeroOne(),getRandZeroOne()];
+	}
+	return [xvel,pixel.velocity[1]+fluxgravity];
+}
+
+function getBump(pixel,array){
+	var chunksize = canv.width/array.length;
+	var index = Math.floor(pixel.x/chunksize);
+	return Math.floor(array[index]/fluxheightscale);
+}
 
 function initFluxPixels() {
-	var numpix =30000;
+	var numpix =20000;
 	addRandomPixelsFlux(numpix)
 }
 
 function getCoord(pixel) {
-	var xval = pixel.x-centX;
-	var yval = pixel.y-centY;
+	var xval = pixel.x-fluxCentX;
+	var yval = pixel.y-fluxCentY;
 	return {"x":xval, "y":yval};
 }
 
 // returns new pixel one step away accord. to pixel's velocity
 function moveStep(pixel) {
-	var spawnRad = 40;
-	pixel.x += pixel.velocity[0];
-	pixel.y += pixel.velocity[1];
-	if(outOfBounds(pixel)) {
-		//pixel.x = randomVal(0,canv.width);
-		//pixel.y = randomVal(0,canv.height);
-		pixel.x -= pixel.velocity[0];
-		pixel.y -= pixel.velocity[1];
-		pixel.velocity[0] = (-1)*pixel.velocity[0];
-		pixel.velocity[1] = (-1)*pixel.velocity[1];
-	}
+	pixel.x += Math.floor(pixel.velocity[0]);
+	pixel.y += Math.floor(pixel.velocity[1]);
 	pixel.coord = getCoord(pixel);
 	pixel.index = calculateIndex(pixel);
+	pixel = setRadAndTheta(pixel);
 	return pixel;
 }
 
@@ -198,28 +364,28 @@ function updateRandomPixelsFlux(array) {
 
 function addRandomPixelsFlux(num) {
 	for(var i=0;i<num;i++){
-		var index = Math.floor(Math.random()*deadPixels.length);
-		var pixel = deadPixels[index];
-		var lastPixel = deadPixels[deadPixels.length-1];
-		deadPixels[index] = lastPixel;
-		deadPixels.pop();
+		var index = Math.floor(Math.random()*fluxDeadPixels.length);
+		var pixel = fluxDeadPixels[index];
+		var lastPixel = fluxDeadPixels[fluxDeadPixels.length-1];
+		fluxDeadPixels[index] = lastPixel;
+		fluxDeadPixels.pop();
 		if(pixel) {
-			livePixels.push(pixel);
-			imageData.data[pixel.index+3] = 255;
+			fluxLivePixels.push(pixel);
+			fluxImageData.data[pixel.index+3] = 255;
 		}	
 	}
 }
 
 function removeRandomPixels(num, note) {
 	for(var i=0;i<num;i++){
-		var index = Math.floor(Math.random()*livePixels.length);
-		var pixel = livePixels[index];
-		var lastPixel = livePixels[livePixels.length-1];
-		livePixels[index] = lastPixel;
-		livePixels.pop();
+		var index = Math.floor(Math.random()*fluxLivePixels.length);
+		var pixel = fluxLivePixels[index];
+		var lastPixel = fluxLivePixels[fluxLivePixels.length-1];
+		fluxLivePixels[index] = lastPixel;
+		fluxLivePixels.pop();
 		if(pixel) {
-			deadPixels.push(pixel);
-			imageData.data[pixel.index+3] = 0;
+			fluxDeadPixels.push(pixel);
+			fluxImageData.data[pixel.index+3] = 0;
 		}
 	}
 }
@@ -235,13 +401,13 @@ function isBlack(pixel) {
 }
 
 function initLiveAndDead() {
-	livePixels = new Array();
-	deadPixels = new Array();
+	fluxLivePixels = new Array();
+	fluxDeadPixels = new Array();
 }
 
 
 function getDistanceFromCenter(x,y) {
-	return Math.sqrt((x-centX)*(x-centX)+(y-centY)*(y-centY));
+	return Math.sqrt((x-fluxCentX)*(x-fluxCentX)+(y-fluxCentY)*(y-fluxCentY));
 }
 
 function initKeyboard() {
@@ -250,7 +416,7 @@ function initKeyboard() {
 		if(code == 49) goFullScreen(); // 1
 		else if(code == 67) toggleCircle(); // C
 		else if(code == 68) toggleDoubleBars(); // D
-		else if(code == 70) toggleFluid();// F
+		else if(code == 70) toggleUpdateFluxPixels();// F
 		else if(code == 65) toggleColor(); // A
 		else if(code == 90) toggleFlippedBars(); // Z
 		else if(code == 77) toggleMiddleBars(); // M
@@ -261,14 +427,22 @@ function initKeyboard() {
 	}
 }
 
-
+function toggleUpdateFluxPixels() {
+	if(fluxUpdatePixelsVal==0) {
+		fluxUpdatePixelsVal++;
+		fluxUpdatePixels = updateFluxRadial;
+	} else {
+		fluxUpdatePixelsVal--;
+		fluxUpdatePixels = updateFluxPixels;
+	}
+}
 
 function catchArrowKey(code) {
 	var scale = 20;
-	if(code == 37) centX -= scale;
-	else if(code == 38) centY -= scale;
-	else if(code==39) centX += scale;
-	else centY += scale;
+	if(code == 37) fluxCentX -= scale;
+	else if(code == 38) fluxCentY -= scale;
+	else if(code==39) fluxCentX += scale;
+	else fluxCentY += scale;
 }
 // a large amplitude is 7 figures
 function getTotalAmplitude(array) {
