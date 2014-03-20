260d32ca21fcb1a664157bfac556ed30e2238a38
katie redmond <katieredmond@katie.stanford.edu>
2014-03-20 11:43:07 -0700

    changed order to avoid switching bugs

--- working/mainkeyboard.js
+++ working/mainkeyboard.js
@@ -12,11 +12,11 @@ function initMainKeyboard() {
 function implementMainKeyboardKeys(code) {
 	if(code == 49) loadFreqBars(); // 1
 	else if(code == 50) loadOscillator(); // 2
-	else if(code == 51) loadNeon(); // 3
-	else if(code == 52) loadRainbowLine(0); // 4
-	else if(code == 53) loadRetro(0); // 5
-	else if(code == 54) loadFlux(); // 6
-	else if(code == 55) loadStarParticles(); // 7
+	else if(code == 51) loadRetro(0); // 3
+	else if(code == 52) loadFlux(); // 4
+	else if(code == 53) loadStarParticles(); // 5
+	else if(code == 54) loadRainbowLine(0); // 6
+	else if(code == 55) loadNeon(); // 7
 	else if(code == 56) loadDonuts(0); // 8
 	else if(code == 57) loadWaves(); // 9	
 }
